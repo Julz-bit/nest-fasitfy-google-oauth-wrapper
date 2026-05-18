d0454172ad04fb9c4f1e3a9013b592d2dd3bf453
Julz-bit <caraanjulz101@gmail.com>
2026-05-18 14:05:26 +0800

    refactor: fn to method

--- src/auth/google-auth.guard.ts
+++ src/auth/google-auth.guard.ts
@@ -8,33 +8,32 @@ export class GoogleAuthGuard extends AuthGuard('google') {
     const reply = http.getResponse();
 
     // Patch Fastify reply to support Express-style methods passport uses
-    patchFastifyReply(reply);
+    this.patchFastifyReply(reply);
 
     const result = await (super.canActivate(context) as Promise<boolean>);
     return result;
   }
+
+  private patchFastifyReply(reply: any) {
+    // Passport calls res.setHeader for redirect
+    if (!reply.setHeader) {
+      reply.setHeader = (name: string, value: string) => {
+        reply.header(name, value);
+      };
+    }
+
+    // Passport calls res.end() to finish the redirect
+    if (!reply.end) {
+      reply.end = () => {
+        reply.send('');
+      };
+    }
+
+    // Passport checks res.headersSent
+    if (reply.headersSent === undefined) {
+      Object.defineProperty(reply, 'headersSent', {
+        get: () => reply.sent,
+      });
+    }
+  }
 }
-
-function patchFastifyReply(reply: any) {
-  // Passport calls res.setHeader for redirect
-  if (!reply.setHeader) {
-    reply.setHeader = (name: string, value: string) => {
-      reply.header(name, value);
-    };
-  }
-
-  // Passport calls res.end() to finish the redirect
-  if (!reply.end) {
-    reply.end = () => {
-      reply.send('');
-    };
-  }
-
-  // Passport checks res.headersSent
-  if (reply.headersSent === undefined) {
-    Object.defineProperty(reply, 'headersSent', {
-      get: () => reply.sent,
-    });
-  }
-}
-
